0c3029907605d2fc82b1c95c58592e86621bb493
Severin Messenbrink <severin@messenbrink.eu>
2024-08-23 10:23:05 +0200

    Update font packages

--- home/common/core/fonts.nix
+++ home/common/core/fonts.nix
@@ -4,25 +4,23 @@
     (nerdfonts.override {
       fonts = ["FiraCode" "CascadiaCode" "FantasqueSansMono"];
     })
-    # adobe-source-code-pro-fonts
+    source-code-pro
     font-awesome
     ia-writer-duospace
     ia-writer-quattro
-    inter-font
     libre-baskerville
     monaspace
     noto-fonts
     noto-fonts-emoji
-    otf-firamono-nerd
-    otf-sora
+    dotcolon-fonts
     papirus-icon-theme
     recursive
     sn-pro
-    ttf-comfortaa
-    ttf-fantasque-nerd
-    ttf-icomoon-feather
-    ttf-iosevka-nerd
-    ttf-jetbrains-mono-nerd
-    ttf-nerd-fonts-symbols-common
+    comfortaa
+    fantasque-sans-mono
+    icomoon-feather
+    iosevka
+    jetbrains-mono
+    nerdfonts
   ];
 }
