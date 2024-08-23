04b3ed1183d3e360c7d49104f9407da290ace851
Severin Messenbrink <severin@messenbrink.eu>
2024-08-23 19:48:09 +0200

    WIP

--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -21,7 +21,6 @@
     overlays = builtins.attrValues outputs.overlays;
     config = {
       allowUnfree = true;
-      allowBroken = true;
       permittedInsecurePackages = [
         "electron-28.3.3"
         "electron-27.3.11"
