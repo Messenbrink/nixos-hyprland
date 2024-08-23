58522355d5bd010f5e371c4255b81ffad1e0bc57
Severin Messenbrink <severin@messenbrink.eu>
2024-08-23 19:33:52 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.config.allowBroken = true;
 
     # TODO: Apply fixes to nixos-precision
     hardware.url = "github:nixos/nixos-hardware";
