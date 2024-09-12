26fc58f248a4d62862fd24bc662ac43be21c99cb
Severin Messenbrink <severin@messenbrink.eu>
2024-09-12 14:41:09 +0200

    re-enable linux-only and switch to nvidia beta driver

--- hosts/nixos-precision/default.nix
+++ hosts/nixos-precision/default.nix
@@ -64,7 +64,7 @@
       powerManagement.finegrained = false;
       open = false;
       nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.latest;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
       prime = {
         offload = {
           enable = true;
